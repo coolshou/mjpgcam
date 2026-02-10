5dd5069e73c413d4c367750fbd8e83860bf023a4
coolshou <jimmy_yeh@alphanetworks.com>
2026-02-10 16:42:08 +0800

    add debian files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/mjpgcam/issues
+# Bug-Submit: https://github.com/<user>/mjpgcam/issues/new
+# Changelog: https://github.com/<user>/mjpgcam/blob/master/CHANGES
+# Documentation: https://github.com/<user>/mjpgcam/wiki
+# Repository-Browse: https://github.com/<user>/mjpgcam
+# Repository: https://github.com/<user>/mjpgcam.git
